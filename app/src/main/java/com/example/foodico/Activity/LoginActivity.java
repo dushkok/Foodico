e44ae9ee345e26876adc04fc9c4123f9fb5174ff
dushkok <duskokasnioski@yahoo.com>
2019-05-18 20:17:41 +0200

    Signup and login added

---
+++ app/src/main/java/com/example/foodico/Activity/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.example.foodico.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.foodico.Model.User;
+import com.example.foodico.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.loginEmailInput)
+    EditText loginEmailInput;
+
+    @BindView(R.id.loginPasswordInput)
+    EditText loginPasswordInput;
+
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        User user = (User) getIntent().getSerializableExtra("signedupUser");
+        if (user != null) {
+            loginEmailInput.setText(user.getEmail());
+            loginPasswordInput.setText(user.getPassword());
+        }
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+    }
+
+    @OnClick(R.id.openSignupButton)
+    public void onOpenSignupButtonClick() {
+        Intent openSignUpActivity = new Intent(this, SignupActivity.class);
+        startActivity(openSignUpActivity);
+    }
+
+    @OnClick(R.id.loginButton)
+    public void onLoginButtonClick() {
+        if (validateInput()) {
+            String email = loginEmailInput.getText().toString();
+            String password = loginPasswordInput.getText().toString();
+            Query query = databaseReference.orderByChild("email").equalTo(email);
+            query.addValueEventListener(valueEventListener(email, password));
+        }
+    }
+
+    public ValueEventListener valueEventListener(final String email, final String password) {
+        return new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    User user = dataSnapshot.getChildren().iterator().next().getValue(User.class);
+                    if (user.getEmail().compareTo(email) == 0 &&
+                            user.getPassword().compareTo(password) == 0) {
+                        Toast.makeText(LoginActivity.this, "Successful login", Toast.LENGTH_SHORT).show();
+                    } else {
+                        loginEmailInput.setError("Wrong email or password!");
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Failed login", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+    }
+
+    public boolean validateInput() {
+        if (!loginEmailInput.getText().toString().contains("@")) {
+            loginEmailInput.setError("Invalid email address");
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/foodico/Activity/SignupActivity.java
@@ -0,0 +1,107 @@
+package com.example.foodico.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.foodico.Model.User;
+import com.example.foodico.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @BindView(R.id.signupEmailInput)
+    EditText signupEmailInput;
+
+    @BindView(R.id.signupNameInput)
+    EditText signupNameInput;
+
+    @BindView(R.id.signupPasswordInput)
+    EditText signupPasswordInput;
+
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+    }
+
+    @OnClick(R.id.signupButton)
+    public void onSignupButtonClick() {
+        final User newUser = getUser();
+        Query query = databaseReference.orderByChild("email").equalTo(newUser.getEmail());
+        if (validateInput()) {
+            query.addListenerForSingleValueEvent(valueEventListener(newUser));
+        }
+    }
+
+    public ValueEventListener valueEventListener(final User user) {
+        return new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    signupEmailInput.setError("Email already in use!");
+                } else {
+                    String id = databaseReference.push().getKey();
+                    databaseReference.child(id).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(SignupActivity.this, "Sign up successful!", Toast.LENGTH_LONG).show();
+                            Intent startLoginActivity = new Intent(SignupActivity.this, LoginActivity.class);
+                            startLoginActivity.putExtra("signedupUser", user);
+                            startActivity(startLoginActivity);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(SignupActivity.this, "Error signing up!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+    }
+
+    public User getUser() {
+        String name = signupNameInput.getText().toString();
+        String email = signupEmailInput.getText().toString();
+        String password = signupPasswordInput.getText().toString();
+        return new User(name, email, password);
+    }
+
+    public boolean validateInput() {
+        if (!signupEmailInput.getText().toString().contains("@")) {
+            signupEmailInput.setError("Invalid email address");
+            return false;
+        } else if (signupNameInput.getText().toString().isEmpty()) {
+            signupNameInput.setError("Name can't be empty");
+            return false;
+        } else if (signupPasswordInput.getText().toString().length() < 5) {
+            signupPasswordInput.setError("Password too short");
+            return false;
+        }
+        return true;
+    }
+}
